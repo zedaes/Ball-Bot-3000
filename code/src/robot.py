a08dc487f38dbb4b40ebbbf2cb7382e0a0eb7a99
zedaes <147769903+zedaes@users.noreply.github.com>
2024-08-10 10:38:52 -0400

    robot mEtHOdS

--- code/src/robot.py
+++ code/src/robot.py
@@ -1,3 +1,18 @@
 class Robot:
     def __init__(self):
         pass
+
+    def main(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def setup(self):
+        pass
+
+    def update(self):
+        pass
+
+    def look(self):
+        pass
