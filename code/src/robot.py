9489b77505d5a48e7fec369cda25ef62388637c7
zedaes <147769903+zedaes@users.noreply.github.com>
2024-08-10 18:21:27 -0400

    add updatePose()

--- code/src/robot.py
+++ code/src/robot.py
@@ -1,4 +1,4 @@
-import math
+from math import *
 from code.src.hardware import gyroscope
 import cv2
 
@@ -13,9 +13,14 @@ from vision.detection import *
 
 
 class Robot:
-    def __init__(self, servos = [], motors = [], cameras = [], gyroscope = Gyroscope()):
+    def __init__(self, servos = [], motors = [], cameras = [], gyroscope = Gyroscope(), wheelRadius = 5, trackWidth = 10, startPose = Pose):
         self.completed = False
         self.alive = True
+
+        self.wheelRadius = wheelRadius
+        self.trackWidth = trackWidth
+
+        self.pose = startPose
 
         self.report = None
 
@@ -62,5 +67,37 @@ class Robot:
             hardware.checkup()
 
     def updatePose(self):
+        ticksPerCount = self.frontLeftMotor.ticksPerCount
+
+        frontLeftMotorTickCount = self.motors[0].tickCount
+        frontRightMotorTickCount = self.motors[1].tickCount
+        backLeftMotorTickCount = self.motors[2].tickCount
+        backRightMotorTickCount = self.motors[3].tickCount
+
         for motor in self.motors:
             motor.updateTickCount()
+
+        deltaFrontLeftMotorTickCount = frontLeftMotorTickCount - self.motors[0].tickCount
+        deltaFrontRightMotorTickCount = frontRightMotorTickCount - self.motors[1].tickCount
+        deltaBackLeftMotorTickCount = backLeftMotorTickCount - self.motors[2].tickCount
+        deltaBackRightMotorTickCount = backRightMotorTickCount - self.motors[3].tickCount
+
+        deltaFrontLeftDistance = (deltaFrontLeftMotorTickCount / ticksPerCount) * (2 * 3.14159 * self.wheelRadius)
+        deltaFrontRightDistance = (deltaFrontRightMotorTickCount / ticksPerCount) * (2 * 3.14159 * self.wheelRadius)
+        deltaBackLeftDistance = (deltaBackLeftMotorTickCount / ticksPerCount) * (2 * 3.14159 * self.wheelRadius)
+        deltaBackRightDistance = (deltaBackRightMotorTickCount / ticksPerCount) * (2 * 3.14159 * self.wheelRadius)
+
+        deltaFrontDistance = (deltaFrontLeftDistance + deltaFrontRightDistance) / 2
+        deltaBackDistance = (deltaBackLeftDistance + deltaBackRightDistance) / 2
+
+        deltaDistance = (deltaFrontDistance + deltaBackDistance) / 2
+        deltaTheta = (deltaFrontRightDistance - deltaFrontLeftDistance) / self.trackWidth
+
+        self.pose.x += deltaDistance * cos(self.theta + deltaTheta / 2)
+        self.pose.y += deltaDistance * sin(self.theta + deltaTheta / 2)
+        self.pose.heading += deltaTheta
+
+        if self.theta > 3.14159:
+            self.theta -= 2 * 3.14159
+        elif self.theta < -3.14159:
+            self.theta += 2 * 3.14159
